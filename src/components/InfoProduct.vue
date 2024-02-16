c60a2e5fd54dd37b818df090a98be876a69f0dc4
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-02-16 09:20:49 -0300

    feat: adicionando mock de componente de info produtos/ refactoring: inputs formulario de dados do cliente

---
+++ src/components/InfoProduct.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+<template>
+    <v-container>
+        <h4>Resumo da compra</h4>
+        <div class="separator"></div>
+        <div class="d-flex justify-space-between">
+            <span>Produto:</span>
+            <span>notebook asus</span>
+        </div>
+        <div class="d-flex justify-space-between">
+            <span>Preço:</span>
+            <span>1000</span>
+        </div>
+        <div class="d-flex justify-space-between">
+            <span>Frete:</span>
+            <span class="text-green">Grátis</span>
+        </div>
+        <div class="separator"></div>
+        <div class="d-flex justify-space-between">
+            <span>Total:</span>
+            <span>1000</span>
+        </div>
+
+        
+
+    </v-container>
+   
+</template>
+<style scoped>
+.separator {
+    background-color: rgb(0 0 0 / 10%);
+    height: 1px;
+    width: 100%;
+    margin-top: 20px;
+}
+    
+</style>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,73 @@
+<script setup>
+import SplitScreen from "../components/SplitScreen.vue"
+import InfoProduct from "../components/InfoProduct.vue";
+</script>
+
+<template>
+  <SplitScreen title="Dados do Cliente">
+    <template #left>
+      <v-card>
+        <v-container>
+          <v-form>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field label="Nome Completo"
+                              variant="outlined" />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field variant="outlined"
+                              label="Número (opcional)" />
+              </v-col>
+              <v-col cols="12" md="10">
+                <v-text-field label="Email"
+                              variant="outlined" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" md="8">
+                <v-text-field label="CEP"
+                              variant="outlined" />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field label="Estado"
+                              variant="outlined" 
+                              disabled />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field label="Cidade" 
+                              variant="outlined"
+                              disabled />
+              </v-col>
+              <v-col cols="12" md="7">
+                <v-text-field label="Bairro"
+                              variant="outlined" />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field label="Rua/Avenida"
+                              variant="outlined" />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field label="Número"
+                              variant="outlined" />
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field label="Complemento (opcional)"
+                               variant="outlined" />
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-container>
+      </v-card>
+      <div class="d-flex justify-end mt-4">
+        <v-btn color="success">Continuar</v-btn>
+      </div>
+    </template>
+
+    <template #rigth>
+      <InfoProduct></InfoProduct>
+    </template>
+
+  </SplitScreen>
+</template>
+
+<style scoped></style>
